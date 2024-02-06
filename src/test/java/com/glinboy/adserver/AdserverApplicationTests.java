b6b1d996c457bd5527e39fa8576aded3547a3eed
GLinBoy <hojjatabedie@gmail.com>
2024-02-06 20:15:12 +0100

    🕯 START: Initiated the project
    resolved #1

---
+++ src/test/java/com/glinboy/adserver/AdserverApplicationTests.java
@@ -0,0 +1,13 @@
+package com.glinboy.adserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AdserverApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
